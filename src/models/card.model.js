35968f21ef2b75d62d5fb75bf10f67f7d1780fd9
Jeff Labonte <jeff.labonte12@gmail.com>
2018-05-27 22:59:06 -0400

    Add mongoose-unique-validator to card

--- src/models/card.model.js
+++ src/models/card.model.js
@@ -1,10 +1,13 @@
 import { mongoose } from "../repository/common.repo";
 import { model } from "mongoose";
+var mongooseUniqueValidator = require('mongoose-unique-validator');
 var Schema = mongoose.Schema;
 
 var cardSchema = new Schema({
     piece : { type: Schema.ObjectId, ref: 'Piece', required: true}
 });
+
+cardSchema.plugin(mongooseUniqueValidator);
 
 var Card = mongoose.model('Card', cardSchema);
 export { Card };
