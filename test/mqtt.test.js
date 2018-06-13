a905199588c586aa1bf1576ca7e02df0d90b2e13
Jeff Labonte <jeff.labonte12@gmail.com>
2018-06-12 22:01:48 -0400

    Add connection to MQTT Broker, Change mqtt framework to communicate with broker
    Remove PAHO MQTT from the package.json, add a simple connection to the broker

--- test/mqtt.test.js
+++ test/mqtt.test.js
@@ -1,6 +1,8 @@
+import { MqttAdapter } from "../src/adapter/mqtt/mqttAdapter.adapter";
+
 describe("Communication with MQTT Server", () => {
     var mqttAdapter = null;
-    test('Connect to the MQTT Broker', (done) => {
+    test('Connect to the MQTT Broker', () => {
         try {
             mqttAdapter = new MqttAdapter();
             expect(mqttAdapter).toBeDefined();
